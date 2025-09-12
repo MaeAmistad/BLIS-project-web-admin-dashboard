25a55b8589d80efafc7aec49270a0779ce8071af
MaeAmistad <104638142+MaeAmistad@users.noreply.github.com>
2025-09-12 11:19:13 -0800

    Sidebar

--- blis-admin/src/scenes/global/Sidebar.jsx
+++ blis-admin/src/scenes/global/Sidebar.jsx
@@ -12,7 +12,7 @@ import PersonOutlinedIcon from '@mui/icons-material/PersonOutlined';
 import CalendarOutlinedIcon from '@mui/icons-material/CalendarOutlined';
 import HelpOutlinedIcon from '@mui/icons-material/HelpOutlined';
 import BarChartOutlinedIcon from '@mui/icons-material/BarChartOutlined';
-import PieChartOutlinedIcon from '@mui/icons-material/PieChartOutlined';
+import PieChartOutlineOutlinedIcon from '@mui/icons-material/PieChartOutlineOutlined';
 import TimelineOutlinedIcon from '@mui/icons-material/TimelineOutlined';
 import MenuOutlinedIcon from '@mui/icons-material/MenuOutlined';
 import MapOutlinedIcon from '@mui/icons-material/MapOutlined';
@@ -21,13 +21,27 @@ import MapOutlinedIcon from '@mui/icons-material/MapOutlined';
 const Sidebar = () => {
     const theme = useTheme();
     const colors = tokens(theme.palette.mode);
-    const [isCollapsed, useIsCollapsed] = useState(false);
+    const [isCollapsed, setIsCollapsed] = useState(false);
     const [selected, setSelected] = useState("Dashboard");
 
     return (
         <Box
             sx={{
-                
+                "& .pro-sidebar-inner":{
+                    background: '${color.primary[400]} !important',
+                },
+                "& .pro-icon-wrapper": {
+                    backgroundColor:"transparent !important",
+                },
+                "& .pro-inner-item": {
+                    padding:"5px 35px 5px 20px !important",
+                },
+                "& .pro-inner-item:hover":{
+                    color: "#868dfb !important",
+                },
+                "& .pro-menu-item.active":{
+                    color:"#6870fa !important",
+                },
             }}>
 
             </Box>
