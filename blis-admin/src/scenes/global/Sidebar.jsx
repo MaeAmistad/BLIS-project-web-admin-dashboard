c39d7cd4863ebdd9d6004850aab3f18eb107691a
MaeAmistad <104638142+MaeAmistad@users.noreply.github.com>
2025-09-11 16:08:30 -0800

    Creation of sidebar

--- blis-admin/src/scenes/global/Sidebar.jsx
+++ blis-admin/src/scenes/global/Sidebar.jsx
@@ -1,5 +1,37 @@
+import {useState} from "react";
+import {ProSidebar, Menu, MenuItem} from "react-pro-sidebar";
+import {Box, IconButton, Typography, useTheme} from "@mui/material"
+import {Link} from "react-router-dom";
+import { tokens} from "../../theme";
+import 'react-pro-sidebar/dist/css/style.css';
+import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
+import PeopleOutlinedIcon from '@mui/icons-material/PeopleOutlined';
+import ContactsOutlinedIcon from '@mui/icons-material/ContactsOutlined';
+import ReceiptOutlinedIcon from '@mui/icons-material/ReceiptOutlined';
+import PersonOutlinedIcon from '@mui/icons-material/PersonOutlined';
+import CalendarOutlinedIcon from '@mui/icons-material/CalendarOutlined';
+import HelpOutlinedIcon from '@mui/icons-material/HelpOutlined';
+import BarChartOutlinedIcon from '@mui/icons-material/BarChartOutlined';
+import PieChartOutlinedIcon from '@mui/icons-material/PieChartOutlined';
+import TimelineOutlinedIcon from '@mui/icons-material/TimelineOutlined';
+import MenuOutlinedIcon from '@mui/icons-material/MenuOutlined';
+import MapOutlinedIcon from '@mui/icons-material/MapOutlined';
+
+
 const Sidebar = () => {
-    return  <div>Sidebar</div>
+    const theme = useTheme();
+    const colors = tokens(theme.palette.mode);
+    const [isCollapsed, useIsCollapsed] = useState(false);
+    const [selected, setSelected] = useState("Dashboard");
+
+    return (
+        <Box
+            sx={{
+                
+            }}>
+
+            </Box>
+    )
 }
 
 export default Sidebar;
